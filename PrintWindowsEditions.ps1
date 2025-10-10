e0afe053251d0204632e17b95dc1da2cf2a5bcba
Sergey Malyuk (fogucoco) <malyuk.ss@genlab.llc>
2025-10-10 11:18:40 +0300

    implement PrintWindowsEditions.ps1

---
+++ PrintWindowsEditions.ps1
@@ -0,0 +1,11 @@
+installPath = Read-Host "Enter install.esd/.wim path (e.g., C:\path\to\install.esd)"
+
+if (-not (Test-Path $installPath)) {
+    Write-Error "The specified install.esd/.wim file does not exist."
+    Read-Host "Press Enter to continue"
+    exit 1
+}
+
+Dism /Get-WimInfo /WimFile:$installPath
+
+Read-Host "Press Enter to continue"
