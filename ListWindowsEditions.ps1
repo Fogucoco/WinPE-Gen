aeaf2209a55a88356bfa087026d52b87657865d8
Sergey Malyuk (fogucoco) <malyuk.ss@genlab.llc>
2025-10-10 11:27:43 +0300

    add a little explanation in ListWindowsEditions.ps1

--- ListWindowsEditions.ps1
+++ ListWindowsEditions.ps1
@@ -1,4 +1,5 @@
 Write-Host "This script lists available Windows editions in a given install.esd or install.wim file."
+Write-Host "Please extract the install.esd or install.wim file from a Windows installation ISO,\nthe script can't read directly from the ISO or ISO mounted drive.\n"
 $installPath = Read-Host "Enter install.esd or install.wim path (e.g., C:\path\to\install.esd)"
 
 if (-not (Test-Path $installPath)) {
